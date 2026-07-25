8091cf9e7678d551fca1a0449a34c9bbedb8e35e
khalid altaj <khalid.altaj.94@gmail.com>
2026-07-25 20:06:19 +0400

    Fix base path for github pages repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './khalid-cv', // اجعلها نقطة وشرطة مائلة هكذا لتكون المسارات نسبية وتعمل في أي مسار فرعي
+  base: '/khalid-cv/', // مهم جداً أن يكون هكذا وليس './' ليتطابق مع اسم المستودع تماماً
   build: {
     chunkSizeWarningLimit: 1000,
   }
