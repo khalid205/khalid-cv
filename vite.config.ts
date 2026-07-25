70650b86cb4a21b2713b58fcd1448909cf8bdc5d
khalid altaj <khalid.altaj.94@gmail.com>
2026-07-25 20:12:31 +0400

    Clean build and redeploy for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/khalid-cv/', // مهم جداً أن يكون هكذا وليس './' ليتطابق مع اسم المستودع تماماً
-  build: {
-    chunkSizeWarningLimit: 1000,
-  }
+  base: '/khalid-cv/',
 })
